70fd07fa72e97e01439ba0f18a89eaf425cf5b36
Michael Bessey <besseym@gmail.com>
2013-03-13 22:20:02 -0400

    New sections

---
+++ hci/573/pages/project.php
@@ -0,0 +1,14 @@
+<h4>Final Project</h4>
+<section>
+	<h5>Warship</h5>
+	<ul>
+		<li><a href="<?php echo LINK_PROJECT_PROPOSAL ?>" target="_blank">Project Proposal</a></li>
+		<li><a href="<?php echo LINK_PROJECT_PROTOTYPE ?>" target="_blank">Project Prototype</a></li>
+		<li>Live Website Url: <a href="http://warship.pro" target="_blank">http://warship.pro</a></li>
+		<li><a href="https://github.com/besseym/warship" target="_blank">Git Repository</a></li>
+	</ul>
+	
+	<image style="float: right;" src="<?php echo HCI_573_PROJECT_BASE . 'images/battle.png' ?>">
+	
+	<p>Warship is a web based re-implementation of the classic game Battleship. The purpose of Warship is to create a fun interactive experience that brings back recollections of time spent with this classic board game as well as provide players with new experiences through a modern re-implementation of Battleship's game-play and aesthetics. This game should be enjoyed by all including casual gamers, sentimental board game enthusiasts, and retired navel officers.</p>
+</section>
---
+++ hci/573/project/prototype/battle_victory_files/story.js
@@ -0,0 +1,233 @@
+var story = {
+ "title": "battle_victory",
+ "pages": [
+  {
+   "title": "home",
+   "height": 771,
+   "width": 1024,
+   "image": "1-home.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      32,
+      66,
+      302,
+      132
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      544,
+      200,
+      864,
+      225
+     ]
+    },
+    {
+     "page": 1,
+     "rect": [
+      720,
+      240,
+      865,
+      270
+     ]
+    }
+   ]
+  },
+  {
+   "title": "registration",
+   "height": 771,
+   "width": 1024,
+   "image": "2-registration.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      32,
+      66,
+      302,
+      132
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      572,
+      512,
+      768,
+      541
+     ]
+    }
+   ]
+  },
+  {
+   "title": "profile_select_opponent",
+   "height": 771,
+   "width": 1024,
+   "image": "3-profile_select_opponent.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      32,
+      66,
+      302,
+      132
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      744,
+      343,
+      841,
+      364
+     ]
+    }
+   ]
+  },
+  {
+   "title": "opponents",
+   "height": 771,
+   "width": 1024,
+   "image": "4-opponents.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      32,
+      66,
+      302,
+      132
+     ]
+    },
+    {
+     "page": 4,
+     "rect": [
+      320,
+      210,
+      704,
+      249
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      745,
+      83,
+      777,
+      115
+     ]
+    }
+   ]
+  },
+  {
+   "title": "profile_new_battle",
+   "height": 771,
+   "width": 1024,
+   "image": "5-profile_new_battle.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      32,
+      66,
+      302,
+      132
+     ]
+    },
+    {
+     "page": 5,
+     "rect": [
+      752,
+      528,
+      888,
+      562
+     ]
+    }
+   ]
+  },
+  {
+   "title": "battle",
+   "height": 771,
+   "width": 1024,
+   "image": "6-battle.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      32,
+      66,
+      302,
+      132
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      141,
+      165,
+      342,
+      213
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      915,
+      154,
+      947,
+      210
+     ]
+    }
+   ]
+  },
+  {
+   "title": "battle_resolution_victory",
+   "height": 778,
+   "width": 1024,
+   "image": "7-battle_resolution_victory.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      32,
+      66,
+      302,
+      132
+     ]
+    },
+    {
+     "page": 6,
+     "rect": [
+      141,
+      166,
+      342,
+      214
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      915,
+      155,
+      947,
+      211
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      640,
+      464,
+      736,
+      485
+     ]
+    }
+   ]
+  }
+ ],
+ "highlightLinks": true
+}
